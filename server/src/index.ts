41ec102cd54802bed57c5ae11b9a09bb1af8f2f2
GlebIgnatuk <zhenyasemioshko@gmail.com>
2024-07-24 21:57:07 +0100

    Add scripts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,1 @@
-
-import { createServer } from 'http';
-
-const transport = new WsTransport()
-const server = createServer();
-
-server.on('upgrade', async (request, socket, head) => {
-    const ws = await transport.handleUpgrade(request, socket, head)  
-    ws.send(JSON.stringify({ type: 'HELLO' }))
-})
-
-server.listen(8080, () => {
-    console.log('Listening on port 8080')
-})
+console.log('Hello')
