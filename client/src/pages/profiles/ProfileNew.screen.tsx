3e91eb5de2e3729ceaa91dc025c2e10d64e97d92
GlebIgnatuk <zhenyasemioshko@gmail.com>
2024-10-12 16:07:35 +0300

    Draft: carousel input

--- client/src/pages/profiles/ProfileNew.screen.tsx
+++ client/src/pages/profiles/ProfileNew.screen.tsx
@@ -1,14 +1,16 @@
-import { FormState, ProfileForm } from './ProfileForm'
+// import { FormState, ProfileForm } from './ProfileForm'
+import { ScrollableNumericInput } from './ScrollableNumericInput'
 
 export const ProfileNewScreen = () => {
-    const onSubmit = async (data: FormState) => {
-        console.log(data)
-    }
+    // const onSubmit = async (data: FormState) => {
+    //     console.log(data)
+    // }
 
     return (
         <div className="h-full overflow-y-auto">
             <div className="py-2">
-                <ProfileForm onSubmit={onSubmit} />
+                {/* <ProfileForm onSubmit={onSubmit} /> */}
+                <ScrollableNumericInput min={0} max={90} step={1} />
             </div>
         </div>
     )
