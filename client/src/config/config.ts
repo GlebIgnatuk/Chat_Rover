8e5a3a748f56e3ae441a38498bcfea2d5dc519d2
GlebIgnatuk <zhenyasemioshko@gmail.com>
2024-11-09 14:21:59 +0200

    Generate random profiles

--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -14,8 +14,73 @@ const names = [
     'Fahad Paul',
 ]
 
-export const FAKE_PROFILES = names
-    .map((name, idx) => ({
+export type FakeProfile = {
+    id: number
+    first_name: string
+    last_name: string | null
+    username: string
+    language_code: string
+    is_premium: boolean
+    allows_write_to_pm: boolean
+}
+
+const randomString = (min: number, max: number) => {
+    const length = Math.floor(Math.random() * (max - min)) + min
+    const sequence = Array.from({ length }, () => Math.floor(Math.random() * (122 - 97)) + 97)
+
+    return String.fromCharCode(...sequence)
+}
+
+export const generateFakeProfile = (
+    from: Partial<FakeProfile>,
+): { profile: FakeProfile; encoded: string } => {
+    const username = from.username ?? randomString(4, 10)
+
+    const profile: FakeProfile = {
+        id: from.id ?? Math.floor(Date.now() / 1000),
+        username: username,
+        first_name: from.first_name ?? username,
+        last_name: from.last_name ?? null,
+        allows_write_to_pm: from.allows_write_to_pm ?? true,
+        is_premium: from.is_premium ?? false,
+        language_code: from.language_code ?? 'en',
+    }
+
+    const params = new URLSearchParams()
+    params.set('user', JSON.stringify(profile))
+    params.set('chat_instance', '-1')
+    params.set('chat_type', 'private')
+    params.set('auth_date', Math.floor(Date.now() / 1000).toString())
+    params.set('hash', '-1')
+    params.set('tgWebAppVersion', '7.10')
+    params.set('tgWebAppPlatform', 'web')
+    params.set(
+        'tgWebAppThemeParams',
+        JSON.stringify({
+            bg_color: '#212121',
+            button_color: '#8774e1',
+            button_text_color: '#ffffff',
+            hint_color: '#aaaaaa',
+            link_color: '#8774e1',
+            secondary_bg_color: '#181818',
+            text_color: '#ffffff',
+            header_bg_color: '#212121',
+            accent_text_color: '#8774e1',
+            section_bg_color: '#212121',
+            section_header_text_color: '#8774e1',
+            subtitle_text_color: '#aaaaaa',
+            destructive_text_color: '#ff595a',
+        }),
+    )
+
+    return {
+        profile: profile,
+        encoded: params.toString(),
+    }
+}
+
+export const FAKE_PROFILES = names.map((name, idx) =>
+    generateFakeProfile({
         id: idx + 1,
         first_name: name.split(' ')[0],
         last_name: name.split(' ')[1] || null,
@@ -23,37 +88,5 @@ export const FAKE_PROFILES = names
         language_code: 'en',
         is_premium: true,
         allows_write_to_pm: true,
-    }))
-    .map((p) => {
-        const params = new URLSearchParams()
-        params.set('user', JSON.stringify(p))
-        params.set('chat_instance', '-1')
-        params.set('chat_type', 'private')
-        params.set('auth_date', Math.floor(Date.now() / 1000).toString())
-        params.set('hash', '-1')
-        params.set('tgWebAppVersion', '7.10')
-        params.set('tgWebAppPlatform', 'web')
-        params.set(
-            'tgWebAppThemeParams',
-            JSON.stringify({
-                bg_color: '#212121',
-                button_color: '#8774e1',
-                button_text_color: '#ffffff',
-                hint_color: '#aaaaaa',
-                link_color: '#8774e1',
-                secondary_bg_color: '#181818',
-                text_color: '#ffffff',
-                header_bg_color: '#212121',
-                accent_text_color: '#8774e1',
-                section_bg_color: '#212121',
-                section_header_text_color: '#8774e1',
-                subtitle_text_color: '#aaaaaa',
-                destructive_text_color: '#ff595a',
-            }),
-        )
-
-        return {
-            user: p,
-            encoded: params.toString(),
-        }
-    })
+    }),
+)
--- client/src/pages/auth/signin/SignIn.screen.tsx
+++ client/src/pages/auth/signin/SignIn.screen.tsx
@@ -1,6 +1,6 @@
 // import loadingScreenImage from '@/assets/loading-screen.webp'
 import signupImage from '@/assets/signup.webp'
-import { FAKE_PROFILES } from '@/config/config'
+import { FAKE_PROFILES, generateFakeProfile } from '@/config/config'
 
 import { useAuth } from '@/context/auth/useAuth'
 import { buildUrl } from '@/utils/url'
@@ -64,6 +64,8 @@ export const SignInScreen = () => {
         return <Navigate to={buildUrl('/auth/signup')} replace />
     }
 
+    const random = generateFakeProfile({})
+
     if (error) {
         return (
             <div className="relative h-full flex justify-center items-center">
@@ -84,16 +86,23 @@ export const SignInScreen = () => {
                         <div className="grid grid-cols-2 gap-2">
                             {FAKE_PROFILES.map((p, idx) => (
                                 <a
-                                    key={p.user.username}
+                                    key={p.profile.username}
                                     href={`/#tgWebAppData=${encodeURIComponent(p.encoded)}`}
                                     className="p-2 rounded-md cursor-pointer"
                                     style={{
                                         backgroundColor: `hsl(${(idx / FAKE_PROFILES.length) * 360}deg, 60%, 50%)`,
                                     }}
                                 >
-                                    {p.user.id}. {p.user.first_name} {p.user.last_name}
+                                    {p.profile.id}. {p.profile.first_name} {p.profile.last_name}
                                 </a>
                             ))}
+
+                            <a
+                                href={`/#tgWebAppData=${encodeURIComponent(random.encoded)}`}
+                                className="p-2 rounded-md cursor-pointer bg-black text-white col-span-2"
+                            >
+                                {random.profile.first_name} {random.profile.last_name} (random)
+                            </a>
                         </div>
                     )}
                 </div>
